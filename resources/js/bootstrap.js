366eafc14476b5f0524b391ebc492bb68a7f685e
Janos Apro <apro.janos@evista.hu>
2025-01-27 16:29:06 +0100

    toastify added

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
 window.axios = axios;
+import Toastify from 'toastify-js';
+window.Toastify = Toastify;
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
